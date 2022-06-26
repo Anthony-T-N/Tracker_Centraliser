f7276f51bd72724ad969e0289723279c636a3037
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2022-06-26 21:21:21 +1000

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <filesystem>
+
+int main()
+{
+    std::cout << "=======================================" << "\n";
+    std::cout << "- Tracker_Centralisation GUI application" << "\n";
+    std::cout << "- GUI Application Version: 1.0" << "\n";
+    std::cout << "- Created By: Anthony-T-N." << "\n";
+    std::cout << "- Current location of executable: " << std::filesystem::current_path() << "\n";
+    std::cout << "=======================================" << "\n\n";
+}
+
+/*
+Project Plan and Design
+
+Brief :
+
+[Version 1]
+
+=== Minimum Functions ===
+[-]
+
+*/
