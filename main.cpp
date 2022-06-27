96bacc64d1e55108a9a2704240e6c338e6c0957e
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2022-06-27 22:20:21 +1000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 #include <filesystem>
 
+#pragma warning(disable:4996);
+
+std::string get_current_date()
+{
+    // https://stackoverflow.com/questions/16357999/current-date-and-time-as-string/16358264
+    time_t rawtime;
+    struct tm* timeinfo;
+    char buffer[80];
+    time(&rawtime);
+    // Error C4996 'localtime': This function or variable may be unsafe.Consider using localtime_s instead.To disable deprecation, use _CRT_SECURE_NO_WARNINGS.
+    timeinfo = localtime(&rawtime);
+    strftime(buffer, sizeof(buffer), "%Y-%m-%d", timeinfo);
+    return buffer;
+}
+
 int main()
 {
     std::cout << "=======================================" << "\n";
@@ -9,16 +24,21 @@ int main()
     std::cout << "- Created By: Anthony-T-N." << "\n";
     std::cout << "- Current location of executable: " << std::filesystem::current_path() << "\n";
     std::cout << "=======================================" << "\n\n";
+
+    std::cout << get_current_date() << "\n";
 }
 
 /*
 Project Plan and Design
 
-Brief :
+Brief : Centralise tracking of a number of activities.
 
 [Version 1]
 
 === Minimum Functions ===
-[-]
+[-] Window that associate entered text/string with a time/date.
+[-] Entered text recorded in csv file.
+[-] Seperate buttons open seperate windows and accept different types of records.
+[-] Optimized to be lightweight and run in the background.
 
 */
