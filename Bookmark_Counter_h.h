e28ff1a9291324e7d317fa0683cd29ffbb0d54e6
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2022-07-17 13:22:43 +1000

    Update Bookmark_Counter_h.h

--- Bookmark_Counter_h.h
+++ Bookmark_Counter_h.h
@@ -6,8 +6,7 @@
 int calculate_difference(int current_bookmark_total_input);
 void remove_csv_last_line();
 int write_to_csv(std::string current_date, int current_bookmark_total_input);
-std::string user_input_validation();
-int bookmark_counter_main();
+int bookmark_counter_main(int test);
 std::string get_current_date();
 
 #endif
