aa9b860c8152e6e46c75afa6ab2e968e49534982
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2022-07-23 21:06:11 +1000

    Add files via upload

---
+++ CSV_Maintainer.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <string>
+
+// Create file -> Get date -> Add event/item -> Close
+
+/*
+   Date | Series Name
+   Date | Events
+   Date | URLS
+
+   Directory Tree:
+   .exe
+   -> Root_folder
+			-> file1.csv
+			-> file2.csv
+			-> file3.csv
+*/
