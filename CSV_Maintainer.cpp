b3c844c4bf791e552819f150685ad0e71dacd918
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2022-07-24 16:11:21 +1000

    Update CSV_Maintainer.cpp

--- CSV_Maintainer.cpp
+++ CSV_Maintainer.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <filesystem>
 #include <string>
+#include <wx/log.h>
+#include "Bookmark_Counter_h.h"
 
 // Create file -> Get date -> Add event/item -> Close
 
@@ -17,3 +19,20 @@
 			-> file2.csv
 			-> file3.csv
 */
+void root_folder_creation(std::string csv_file_name)
+{
+    std::string root_folder_name("_Tracking_Centraliser_Root_Folder");
+    if (std::filesystem::exists(root_folder_name) == false)
+    {
+        wxLogMessage("[-] _Tracking_Centraliser_Root_Folder does not exist;");
+        std::filesystem::create_directories(root_folder_name);
+    }
+    std::ofstream output_file;
+    if (std::filesystem::exists(root_folder_name + "/" + csv_file_name) == false)
+    {
+        const wxString& temp_log_message = "[-]" + root_folder_name + "/" + csv_file_name;
+        wxLogMessage(temp_log_message);
+        output_file.open(root_folder_name + "/" + csv_file_name);
+        wxLogMessage("[+] Created " + temp_log_message + "successfully;");
+    }
+}
