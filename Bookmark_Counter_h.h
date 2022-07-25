9196eacbe068aa56bce8371057be3db8692f9c43
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2022-07-25 21:30:15 +1000

    Update Bookmark_Counter_h.h

--- Bookmark_Counter_h.h
+++ Bookmark_Counter_h.h
@@ -8,6 +8,7 @@ int bookmark_counter_main(int test);
 std::string get_current_date();
 
 //CSV_Maintainer.cpp
-void root_folder_creation(std::string csv_file_name);
+int csv_maintainer_main(std::string csv_file_name, std::string message);
+void root_folder_creation(std::string root_folder_name, std::string csv_file_name);
 
 #endif
