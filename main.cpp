a6925e29397980a4866ce5536c9f7456ca1f9c2a
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2022-11-05 19:53:00 +1100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -272,6 +272,7 @@ void Main_Frame::insert_to_csv(std::string category_label, std::string text_date
 
     //Main_Frame::UpdateStatusBar(std::filesystem::current_path().generic_string() + "/" + "_Tracking_Centraliser_Root_Folder" + "/" + category_label + ".csv"); #Latest update to file.
     Main_Frame::UpdateStatusBar("[+] " + category_label + ".csv" + " | " + text_date_field  + " | " + text_record_field);
+    Main_Frame::SetStatusBarPane(-1); // Prevent wxStatusBar from disappearing.
     wxLogMessage("[+] Inserted: [" + text_date_field + "] " + text_record_field + " to " + category_combo_box->GetValue() + ".csv");
     
     update_category_item_arr();
